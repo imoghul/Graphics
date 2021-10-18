3ede78d103aa819d8f9f57b3eac56108870a350a
imoghul <moghul02@gmail.com>
2021-10-18 11:16:41 -0400

    bux fixes

--- Drawer.java
+++ Drawer.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 import javax.swing.*;
-import java.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
--- Shape.java
+++ Shape.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 import javax.swing.*;
-import java.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
